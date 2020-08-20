031b3ea35e673f14cd165bec0f83011885817710
RanMaou <55929611+RanMaou@users.noreply.github.com>
2020-08-20 21:30:07 +0300

    1

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert("Hello, World!");
+const firstRow = 'мама мыла раму';
+const secondRow = 'собака друг человека';
+
+function countA(row) 
+{
+    let a = 0;
+    for (let i = 0; i < row.length; i++) 
+    { 
+      if (row[i]=='а') a++;
+    }
+    return a;
+}
+
+function getRow(firstRow, secondRow) 
+{
+   if (countA(firstRow) > countA(secondRow)) return firstRow;
+    else return secondRow;
+}
+
+console.log(getRow(firstRow, secondRow)); // мама мыла раму
